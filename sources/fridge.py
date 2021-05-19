6a81347304f5532cc631afb5512c5bb617854c6e
Pantomik <addr@domain.ext>
2021-05-19 10:30:10 +0200

    Fridge can count

--- sources/fridge.py
+++ sources/fridge.py
@@ -35,6 +35,7 @@ class Fridge:
             default_sauce = 0
         self._ingredients: Dict[str, int] = dict.fromkeys(ALL_INGREDIENTS, default_ingredients)
         self._sauces: Dict[str, int] = dict.fromkeys(ALL_SAUCES, default_sauce)
+        self._count: Dict[str, int] = dict.fromkeys(ALL_INGREDIENTS, 0)
 
     @property
     def is_empty(self) -> bool:
@@ -70,14 +71,20 @@ class Fridge:
         for name, amount in ingredients.items():
             self.add_ingredient(name, amount)
 
-    def use_ingredient(self, name: str, amount: int):
+    def use_ingredient(self, name: str, amount: int, count: bool = False):
         if name not in self._ingredients:
             raise UnknownIngredientException(name)
         if self._ingredients[name] < amount:
             raise NotEnoughIngredientException(name)
         if amount <= 0:
             raise ValueError('Amount should be positive')
+        if count is True:
+            self.__count_as_used({name: amount})
         self._ingredients[name] -= amount
+
+    def __count_as_used(self, ingredients: Dict[str, int]):
+        for k, v in ingredients.items():
+            self._count[k] += v
 
     def use_multiple_ingredients(self, ingredients: Dict[str, int]) -> None:
         taken: Dict[str, int] = dict()
@@ -89,4 +96,9 @@ class Fridge:
                 raise e
             else:
                 taken[name] = amount
+        self.__count_as_used(ingredients)
         return
+
+    @property
+    def ingredient_used(self) -> Dict[str, int]:
+        return self._count
